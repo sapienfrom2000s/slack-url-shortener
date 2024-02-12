c8196dbdcde1ebc69fa5fb2cfe9be67f8d7decfd
thirtyone <sk292207@gmail.com>
2024-02-13 04:19:03 +0530

    Make unit tests pass

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'pg'
 
 gem 'slack-ruby-bot-server-events'
 
+gem 'faraday'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,28 @@
+require 'faraday'
+require 'securerandom'
+
 class Url < ApplicationRecord
+  validates :shortened, presence: true
+  validates :original, presence: true
+
+  DOMAIN = 'customdomain.com'.freeze
+
+  def self.generate_alphanumeric_code(length = 5)
+    SecureRandom.alphanumeric(length)
+  end
+
+  def self.valid?(url)
+    Faraday.get(url).success?
+  rescue StandardError
+    false
+  end
+
+  def self.shorten(url)
+    return nil unless valid?(url)
+
+    loop do
+      shortened_link = "https://#{DOMAIN}/#{generate_alphanumeric_code}"
+      return shortened_link unless Url.exists?(shortened: shortened_link)
+    end
+  end
 end
